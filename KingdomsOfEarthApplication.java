3a6511cb80b90c46f814a768a275ae5c108562df
CanoramaProductions <spedooker@gmail.com>
2024-12-22 22:32:40 -0600

    Create KingdomsOfEarthApplication.java

---
+++ KingdomsOfEarthApplication.java
@@ -0,0 +1,11 @@
+package com.kingdomsofearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingdomsOfEarthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KingdomsOfEarthApplication.class, args);
+    }
+}
